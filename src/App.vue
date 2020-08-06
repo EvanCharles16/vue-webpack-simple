1195a8d9a9ee62434858ebce2fd168fe343a313d
EvanCharles16 <evancharles1609@gmail.com>
2020-08-06 15:56:01 +0700

    register component locally

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <template>
-  <app-servers></app-servers>
+  <div class="container">
+    <!-- Header -->
+    <app-header></app-header>
+
+    <hr />
+
+    <div class="row">
+      <!-- Servers -->
+      <app-servers></app-servers>
+      <!-- Server Details -->
+      <app-server-details></app-server-details>
+    </div>
+
+    <hr />
+
+    <!-- Footer -->
+    <app-footer></app-footer>
+  </div>
 </template>
 
 <script>
+import Header from "./Header";
+import Servers from "./Servers";
+import ServerDetails from "./ServerDetails";
+import Footer from "./Footer";
+export default {
+  components: {
+    "app-header": Header,
+    "app-servers": Servers,
+    "app-server-details": ServerDetails,
+    "app-footer": Footer,
+  },
+};
 </script>
 
 <style>
---
+++ src/ServerDetails.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="col-xs-12 col-md-6">
+    <p>Sever Details are currently not updated</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
